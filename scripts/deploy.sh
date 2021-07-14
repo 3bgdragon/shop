3c827021758b5086d99c1164c63714c2cefa3ce4
YeongBin <ybbae@saminsys.co.kr>
2021-07-14 16:02:21 +0900

    Codedeploy 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl shop | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl shop)
 
 echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 
